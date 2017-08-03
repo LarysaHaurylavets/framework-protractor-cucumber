44f75d57872cf80fc8797d5f057cd02e94f0561c
LarysaHaurylavets <larysa_haurylavets@epam.com>
2017-08-03 15:46:40 +0300

    Update gulpfile.js

--- framework/gulpfile.js
+++ framework/gulpfile.js
@@ -2,17 +2,21 @@ var gulp=require('gulp'),
 	cucumber=require('gulp-cucumber'),
 	protractor=require('gulp-protractor').protractor,
 	//webdriver_standalone=require('gulp-protractor').webdriver_standalone;	
-	gutil=require('gulp-util');
+	util=require('gulp-util');
 
 
 //gulp.task('webdriver_standalone', webdriver_standalone);
 
-gulp.task('test',  function(){
-	gutil.env.browser? process.env.BROWSER=gutil.env.browser: process.env.BROWSER='chrome' ;
-	gutil.env.platform?process.env.PLATFORM=gutil.env.platform:process.env.PLATFORM='desktop';
-
-	return gulp.src('features/*')		
-		.pipe(protractor({
-			'configFile': 'protractor.conf.js'
-		}));
+gulp.task('test', function () {
+  process.env.BROWSER = util.env.browser ? util.env.browser : "chrome";
+  process.env.PLATFORM = util.env.platform ? util.env.platform : "desktop";
+  process.env.TAG = util.env.tag ? util.env.tag : "";
+  return gulp.src('features/*')
+    .pipe(protractor({
+      configFile: "protractor.conf.js",
+      args: [
+        '--cucumberOpts.tags', process.env.TAG
+      ]
+    }))
+    .on('error', function (e) { throw e })
 });
